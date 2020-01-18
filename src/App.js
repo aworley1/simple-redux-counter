62556d8afe4ed54410ebcb916b5f70fa1cd29289
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-01-18 21:59:58 +0000

    Initial React version of counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {number: 0};
+    }
+
+    render() {
+        let {number} = this.state;
+
+        const increment = () => {
+            this.setState({number: number + 1});
+        };
+
+        const decrement = () => {
+            this.setState({number: number - 1});
+        };
+
+        return (<Counter increment={increment} decrement={decrement} value={number}/>)
+    }
 }
 
+const Counter = (props) => {
+    return (
+        <div>
+            <div className="counter">
+                {props.value}
+            </div>
+            <button onClick={props.increment}>+</button>
+            <button onClick={props.decrement}>-</button>
+        </div>
+    )
+};
+
 export default App;
+
